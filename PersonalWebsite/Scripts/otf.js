3638576c6d5e84a3749d5c8e6468843d26869cde
Amelio Wright <awright2@paylocity.com>
2015-11-15 09:17:17 -0600

    ui enhancements

--- PersonalWebsite/Scripts/otf.js
+++ PersonalWebsite/Scripts/otf.js
@@ -21,20 +21,20 @@
 $(function () {
 
     // hide #back-top first
-    $("#return-to-top").hide();
+    $("#returnToTop").hide();
 
     // fade in #back-top
     $(function () {
         $(window).scroll(function () {
             if ($(this).scrollTop() > 100) {
-                $('#return-to-top').fadeIn(300);
+                $('#returnToTop').fadeIn(300);
             } else {
-                $('#return-to-top').fadeOut(300);
+                $('#returnToTop').fadeOut(300);
             }
         });
 
         // scroll body to 0px on click
-        $('#return-to-top i').click(function () {
+        $('#returnToTop a').click(function () {
             $('body,html').animate({
                 scrollTop: 0
             }, 500);
